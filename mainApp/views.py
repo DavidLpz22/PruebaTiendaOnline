6aa9b0a7002cfdf23d225b383f8cc3b1c1016772
Benja Espejo <benjamin.espejo03@inacapmail.cl>
2025-11-22 19:11:44 -0300

    Update views.py
    se agrego la preselección de producto en formulario de pedido y mejoras en vista solicitar_pedido

--- mainApp/views.py
+++ mainApp/views.py
@@ -37,4 +37,71 @@ def detalle_producto(request, id):
         'producto': producto,
     }
 
-    return render(request, 'detalle_producto.html', data)   
+    return render(request, 'detalle_producto.html', data)  
+
+def solicitar_pedido(request):
+
+    producto_id = request.GET.get("producto")
+    producto_seleccionado = None
+
+    if producto_id:
+        try:
+            producto_seleccionado = Producto.objects.get(id=producto_id)
+        except Producto.DoesNotExist:
+            producto_seleccionado = None
+
+    if request.method == "POST":
+        nombre = request.POST.get("cliente_nombre")
+        email = request.POST.get("cliente_email", "")
+        telefono = request.POST.get("cliente_telefono", "")
+        red_social = request.POST.get("cliente_red_social", "")
+        producto_ref = request.POST.get("producto_referencia")
+        descripcion = request.POST.get("descripcion")
+        fecha_solicitada = request.POST.get("fecha_solicitada")
+
+        pedido = Pedido.objects.create(
+            cliente_nombre=nombre,
+            cliente_email=email,
+            cliente_telefono=telefono,
+            cliente_red_social=red_social,
+            descripcion=descripcion,
+            fecha_solicitada=fecha_solicitada if fecha_solicitada else None,
+            fecha_creacion=timezone.now().date(),
+            fecha_actualizacion=timezone.now().date(),
+            plataforma_origen="sitio_web",
+            estado="solicitado",
+            estado_pago="pendiente",
+            producto_referencia=Producto.objects.get(id=producto_ref) if producto_ref else None
+        )
+
+        
+        imagenes = request.FILES.getlist("imagenes")
+        for img in imagenes:
+            PedidoImagen.objects.create(
+                pedido=pedido,
+                imagen=img
+            )
+
+        url_seguimiento = f"/pedido/seguimiento/{pedido.token_seguimiento}/"
+
+        return render(
+            request,
+            "pedido_creado.html",
+            {"pedido": pedido, "url_seguimiento": url_seguimiento}
+        )
+
+    return render(request, "crear_pedido.html", { 
+        "productos": Producto.objects.all(),
+        "producto_seleccionado": producto_seleccionado
+    })
+
+def seguimiento_pedido(request, token):
+    try:
+        pedido = Pedido.objects.get(token_seguimiento=token)
+    except Pedido.DoesNotExist:
+        raise Http404("No existe un pedido con este código.")
+
+    return render(request, "seguimiento.html", {
+        "pedido": pedido,
+        "imagenes": pedido.imagenes.all()
+    })
